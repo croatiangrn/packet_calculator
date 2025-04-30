c36ef99bf6109757d4cc53471567ee0757c7f25a
Marko Mikulić <mikulic.marko996@gmail.com>
2025-04-30 15:27:15 +0200

    Added CORS

--- src/infrastructure/http/router.go
+++ src/infrastructure/http/router.go
@@ -2,8 +2,10 @@ package http
 
 import (
 	"github.com/croatiangrn/packet_calculator/src/container"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 var appContainer *container.Container
@@ -15,6 +17,16 @@ func SetContainer(container *container.Container) {
 func InitRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(gin.Recovery())
+
+	// Configure CORS middleware (Gin's official implementation)
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://calculator.mikulic.dev"}, // TODO: This should be moved to a config file
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	v1API := router.Group("/v1")
 	{
