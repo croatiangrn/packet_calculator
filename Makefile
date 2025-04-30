1531231e613a438b8d6f612b7dff05a05cd20726
Marko Mikulić <mikulic.marko996@gmail.com>
2025-04-30 12:37:25 +0200

    Added initial files

---
+++ Makefile
@@ -0,0 +1,21 @@
+DOCKER_COMPOSE = docker compose -f docker/docker-compose.yml --env-file .env
+
+DOCKER = docker
+
+build: ##Build environment
+	$(DOCKER_COMPOSE) build --no-cache
+
+start: ##Start containers in detached mode
+	$(DOCKER_COMPOSE) up -d
+
+stop: ##Stop containers
+	$(DOCKER_COMPOSE) stop
+
+restart: ##Restart running containers
+	make stop && make start
+
+rebuild: ##Rebuild running containers
+	make stop && make build && make start
+
+tests: ##Run tests locally
+	go test -v ./...
--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: http.DefaultServeMux, // Your router here
+	}
+
+	go func() {
+		log.Println("Server starting on :8080")
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Server error: %v\n", err)
+		}
+	}()
+
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown error: %v\n", err)
+	}
+
+	log.Println("Server gracefully stopped")
 }
